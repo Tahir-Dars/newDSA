0b116d192813cdbd69bc969e23f35fe25d80699f
Muhammad Tahir <mtahirdars32@gmail.com>
2025-12-19 17:41:27 +0500

    add: the loic of frequency checking

--- src/Loops/digitFrequency.java
+++ src/Loops/digitFrequency.java
@@ -1,4 +1,19 @@
 package Loops;
 
 public class digitFrequency {
+    public static void main(String[] args) {
+        int number = 1223334444;
+        int digitToCount = 4;
+        int frequency = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            if (lastDigit == digitToCount) {
+                frequency++;
+            }
+            number /= 10;
+        }
+
+        System.out.println("The digit " + digitToCount + " appears " + frequency + " times.");
+    }
 }

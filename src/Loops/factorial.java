615fbe55498e6495fcc29922d9fce444a775ba5e
Muhammad Tahir <mtahirdars32@gmail.com>
2025-12-19 17:55:43 +0500

    add: implement factorial calculation in new class

---
+++ src/Loops/factorial.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class factorial {
+    static void main() {
+        int number = 5;
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("The factorial of " + number + " is " + factorial);
+    }
+}

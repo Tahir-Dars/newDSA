b678c9f133bb4f8f053d65ee896f9d1cdccf5da5
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-24 19:28:51 +0500

    add: refactor twoSum class to improve functionality and output format

--- src/Arrays/twoSum.java
+++ src/Arrays/twoSum.java
@@ -1,16 +1,24 @@
 package Arrays;
-
+import Arrays.ClassforUsedFunctions;
 public class twoSum {
-    public
+    public static int[] twoSum(int[] nums, int target) {
+        int [] arr=new int[2];
+        for (int i=0;i<nums.length;i++){
+            for (int j=i+1;j<nums.length;j++){
+                if (nums[i]+nums[j]==target){
+                    arr[0]=i;
+                    arr[1]=j;
+                }
+            }
+        }
+        return arr;
+    }
+
     static void main() {
-         int [] arr={13,9,3,11,4,7};
-         int target=10;
-         for (int i=0;i< arr.length;i++){
-             for (int j=i+0;j< arr.length;j++){
-                 if (arr[i]+arr[j]==target){
-                     System.out.println("{"+i+","+j+"}");
-                 }
-             }
-         }
+        int[] nums={2,7,11,15};
+        int target=9;
+        for (int value : twoSum(nums,target)) {
+            System.out.println(value);
+        }
     }
 }
